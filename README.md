23dba295bfe78b2dc7d8cf4e9beb2a51d09826e0
Dafydd <me@daf.lol>
2022-09-27 11:48:45 +0100

    Add initial (UNTESTED) kasa support + blinkt

---
+++ README.md
@@ -0,0 +1,3 @@
+# Smart Recording Light
+
+A project to turn a standard LED light (or any other mains appliance) into a recording light controllable by recording in Logic Pro X, connected via MIDI to a Raspberry Pi Zero 2W. The default configuration also includes a visible light strip, via a Pimoroni Blinkt, to supplement the main light.
--- monitor.py
+++ monitor.py
@@ -1,18 +1,31 @@
-import mido
+import mido # MIDI library
+import asyncio # Required for kasa library
+from kasa import SmartPlug # Smart Plug Control
+from blinkt import set_pixel, set_brightness, show, clear # Blinkt LED board controller
+
+plug = SmartPlug("127.0.0.1")
+
 state = False
 # set light state to off by default
 
 with mido.open_input() as inport:
+    # No filtering is done on the MIDI input, because the only MIDI signal being sent to this input is the output of the Logic recording light.
     for msg in inport:
         if msg.type == 'note_on':
-            if state == False:
+            if state == False: # Checks light is off before triggering on signal
+                asyncio.run(plug.turn_on) # Turn plug on
                 print('light on')
+                for i in range(8):
+                    set_pixel(i,255,0,0) # Set 8 pixels of LED board to red
                 state = True
             else:
                 pass
         if msg.type == 'note_off':
-            if state == True:
+            if state == True: # Checks light is off before triggering off signal
+                asyncio.run(plug.turn_off)
                 print('light off')
+                for i in range(8):
+                    set_pixel(i,255,255,255) # Set 8 pixels of LED board to white
                 state = False
             if state == False:
                 pass
